cb09db33a8dc14194917f92a1747a1990c2cd24d
Carlos Daniel Trinidad Gonzalez <carlos.trinidad@ibm.com>
2018-01-24 13:01:41 -0300

    si

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ChatPrincipalModule } from './chat-principal/chat-principal.module';
 import { ChatSecundarioModule } from './chat-secundario/chat-secundario.module';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { ChatSecundarioModule } from './chat-secundario/chat-secundario.module';
   imports: [
     BrowserModule,
     ChatPrincipalModule,
-    ChatSecundarioModule
+    ChatSecundarioModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
