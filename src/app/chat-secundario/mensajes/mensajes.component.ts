e70c22ca5907c1ab932b152c3da031a2d265e7b8
Carlos Daniel Trinidad Gonzalez <carlos.trinidad@ibm.com>
2018-01-24 18:10:54 -0300

    si

--- src/app/chat-secundario/mensajes/mensajes.component.ts
+++ src/app/chat-secundario/mensajes/mensajes.component.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import { Mensaje } from '../../datos/datos-modelo/datos.model';
+import { DatosService } from '../../datos/datos-servicio/datos.service';
 
 @Component({
   selector: 'app-mensajes',
-  templateUrl: './mensajes.component.html',
-  styleUrls: ['./mensajes.component.css']
+  template: `
+  <div class="mensaje">
+    <div *ngFor="let mensaje of mensajes">
+      <span *ngIf="mostrarNombre(mensaje.emisor)" 
+      [ngClass]="getClass(mensaje.emisor)">{{ mensaje.emisor }}</span>
+      <br>
+      <span [ngClass]="getClass(mensaje.emisor)">{{ mensaje.texto }}</span>
+      <br>
+    </div>
+  </div>`,
+  styles: [`
+    .mensaje{
+      width: 40%;
+  }
+  .mensaje-principal{
+      background-color: green;
+      float: left;
+  }
+  .mensaje-secundario{
+      background-color: blue;
+      float: right;
+}`]
 })
 export class MensajesComponent implements OnInit {
 
-  constructor() { }
+  mensajes: Mensaje[];
+
+  constructor(private datosService: DatosService) { }
 
   ngOnInit() {
+    this.mensajes = this.datosService.getMensajes();
+  }
+
+  mostrarNombre(emisor: string): boolean{
+    let mensajeSecundario: boolean = false;
+    if(emisor === 'chat-secundario'){
+      mensajeSecundario = true;
+    }
+    return mensajeSecundario;
+  }
+
+  getClass(emisor: string): string{
+    let clase: string = '';
+    if(emisor === 'chat-secundario'){
+      clase = 'mensaje-secundario';
+    }
+    else if(emisor === 'chat-principal'){
+      clase = 'mensaje-principal';
+    }
+
+    return clase;
   }
 
 }
