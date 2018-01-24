0486f6a8a881a633c1be45f0b905d65d3c3ca927
Carlos Daniel Trinidad Gonzalez <carlos.trinidad@ibm.com>
2018-01-24 12:41:12 -0300

    si

--- src/app/chat-principal/input-mensaje/input-mensaje.component.ts
+++ src/app/chat-principal/input-mensaje/input-mensaje.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DatosService } from '../../datos/datos-servicio/datos.service';
+import { Mensaje } from '../../datos/datos-modelo/datos.model';
 
 @Component({
   selector: 'app-input-mensaje',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputMensajeComponent implements OnInit {
 
-  constructor() { }
+  //Creamos el objeto mensaje
+  mensaje: Mensaje;
+  //Instanciamos en el servidor el servicio
+  constructor(private datosService: DatosService) { }
 
   ngOnInit() {
+    //Ya recibido el servicio de clientes, generamos un nuevo mensaje
+    //como emisor el chat principal y texto vacio
+    this.mensaje = this.datosService.nuevoMensajePrincipal();
   }
 
+  //Generamos el metodo para enviar el mensaje al servicio
+  enviarMensaje(){
+    //Llamamos al metodo agregarMensajes() del servicio y le pasamos
+    //nuestro objeto mensaje para que lo agrege al array de mensajes
+    this.datosService.agregarMensajes(this.mensaje);
+    //Inicializamos nuevamente el objeto mensaje en vacio.
+    this.mensaje = this.datosService.nuevoMensajePrincipal();
+  }
 }
