b79cdc5712292d3fe59058fc51957dcacc5e6c94
Carlos Daniel Trinidad Gonzalez <carlos.trinidad@ibm.com>
2018-01-24 13:25:46 -0300

    si

--- src/app/chat-secundario/input-mensaje/input-mensaje.component.ts
+++ src/app/chat-secundario/input-mensaje/input-mensaje.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { DatosService } from '../../datos/datos-servicio/datos.service';
+import { Mensaje } from '../../datos/datos-modelo/datos.model';
 
 @Component({
   selector: 'app-input-mensaje',
-  templateUrl: './input-mensaje.component.html',
+  template: `
+    <div class="zona-input-mensaje">
+      <input type="text" [(ngModel)]="mensaje.texto" (keyup.enter)="enviarMensaje()">
+      <button (click)="enviarMensaje()">Enviar</button>
+    </div>
+  `,
   styleUrls: ['./input-mensaje.component.css']
 })
 export class InputMensajeComponent implements OnInit {
 
-  constructor() { }
+  mensaje: Mensaje;
+  constructor(private datosService: DatosService) { }
 
   ngOnInit() {
+    //Ya recibido el servicio de clientes, generamos un nuevo mensaje
+    //como emisor el chat SECUNDARIO y texto vacio
+    this.mensaje = this.datosService.nuevoMensajeSecundario();
   }
 
+  enviarMensaje(){
+    this.datosService.agregarMensajes(this.mensaje);
+    //Inicializamos nuevamente el objeto mensaje en vacio con emisor SECUNDARIO
+    this.mensaje = this.datosService.nuevoMensajeSecundario();
+  }
 }
